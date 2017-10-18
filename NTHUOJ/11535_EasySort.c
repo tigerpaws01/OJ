fef4e6440efa11b8b1f0762a192638a69eee1ddb
tigerpaws01 <31960225+tigerpaws01@users.noreply.github.com>
2017-10-19 00:41:48 +0800

    Create 11535_EasySort.c

---
+++ NTHUOJ/11535_EasySort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b)
+{
+	int _a = *(int*)a,
+			_b = *(int*)b;
+	return _a > _b;
+}
+
+int main()
+{
+	int n, array[100000], k;
+	scanf("%d", &n);
+	for(k = 0; k<n; k++) scanf("%d", &array[k]);
+	qsort(array, n, sizeof(int), cmp);
+	for(k = 0; k<n; k++) printf("%d ", array[k]);
+}
