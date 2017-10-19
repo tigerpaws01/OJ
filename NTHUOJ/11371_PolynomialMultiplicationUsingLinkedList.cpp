5d817883862bd08fa630d3515d5524dc941e1428
tigerpaws01 <31960225+tigerpaws01@users.noreply.github.com>
2017-10-19 19:50:10 +0800

    Update 11371_PolynomialMultiplicationUsingLinkedList.cpp

--- NTHUOJ/11371_PolynomialMultiplicationUsingLinkedList.cpp
+++ NTHUOJ/11371_PolynomialMultiplicationUsingLinkedList.cpp
@@ -1,139 +1,100 @@
-#include <cstdio>
-#include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include "function.h"
 using namespace std;
 
-Node * head;
 
-void printNode(Node* head);
-
-void _insert(Node *&sequence, long data, int power)
+void printNode(Node* head)
 {
-	Node * s = sequence;
-	// When the sequence is empty
-	if(s == NULL)
-	{
-		//printf("  Sequence is empty\n");
-		sequence = (Node*)malloc(sizeof(Node));
-		sequence->data = data;
-		sequence->power = power;
-		sequence->next = NULL;
-		sequence->prev = NULL;
-		return;
-	}
-	// Insert in the front of the sequence
-	if(s->power < power)
-	{
-		//printf("  Front of it\n");
-		sequence->prev =(Node*)malloc(sizeof(Node));
-		sequence->prev->next = sequence;
-		sequence->prev->data = data;
-		sequence->prev->power = power;
-		sequence = sequence->prev;
-	}
-	// Same as the sequence head
-	else if (sequence->power == power)
-	{
-		sequence->data += data;
-		return;
-	}
-	else //s->power > power
-	{
-		for(;s->next != NULL && s->next->power > power;s = s->next);
-		if(s->next == NULL)
-		{
-			//printf("  To the last\n");
-			s->next = (Node*)malloc(sizeof(Node));
-			s->next->next = NULL;
-			s->next->prev = s;
-			s->next->data = data;
-			s->next->power = power;
-		}
-		else
-		{
-			if(s->next->power == power)
-			{
-				//printf("  Existed!\n");
-				s->next->data += data;
-			}
-			else // s->next->power < power
-			{
-				Node * newNode = (Node*)malloc(sizeof(Node));
-				newNode->data = data;
-				newNode->power = power;
-				newNode->next = s->next;
-				newNode->prev = s;
-				s->next->prev = newNode;
-				s->next = newNode;
-			}
-		}
-	}
-	
-	
+	for(;head != NULL;head = head->next)	
+		if(head->data != 0) cout << " " << head->data << " " << head->power;
 }
 
-void printNode(Node* head)         //the function which will print the output
+Node* create()
 {
-	while(head != NULL)
-	{
-		if(head->data == 0)
-		{
-			head = head->next; continue;
-		}
-		cout << " " << head->data << " " << head->power;
-		head = head->next;
-	}
-}
-Node* create()                     //create polynomial linked list
-{
-	long data;
-	int power;
-	head = (Node*)malloc(sizeof(Node));
-	
-	head->next = NULL;
-	head->prev = NULL;
-	Node * cur = head;
+	Node *head = (Node*)malloc(sizeof(Node)), *temp;
+	temp = head;
 	while(1)
 	{
-		int d, p;
-		cin >> d >> p;
-		cur->data = d;
-		cur->power = p;
+		cin >> temp->data >> temp->power;
+		temp->next = NULL;
+		if(temp->power == 0) break;
 		
-		if(cur->power == 0) break;
-		
-		cur->next = (Node*)malloc(sizeof(Node));
-		cur->next->next = NULL;
-		cur->next->prev = cur;
-		cur = cur->next;
+		temp->next = (Node*)malloc(sizeof(Node));
+		temp->next->prev = temp;
+		temp = temp->next;
 	}
-	//cout << head->data << "\n";
 	return head;
 }
-void destroy(Node *node)           //destroy polynomial linked list
+void destroy(Node *node)
 {
-	Node * cur = node;
-	while(cur != NULL)
+	while(node != NULL)
 	{
-		Node * toD = cur;
-		cur = cur->next;
-		free(toD);
+		Node *tod = node;
+		node = node->next;
+		free(tod);
 	}
 }
-Node* multiple(Node* p1, Node* p2) //input two polynomial linked list and return the result
+
+void _insert(Node *&head, long data, int power)
 {
-	Node * resultHead = NULL;
-	Node * curP1 = p1, *curP2 = p2;
-	for(;curP1 != NULL;curP1 = curP1->next)
+	Node *cur = head;
+	if(cur == NULL)
 	{
-		curP2 = p2;
-		for(;curP2 != NULL;curP2 = curP2->next)
+		head =(Node*)malloc(sizeof(Node));
+		head->data = data;
+		head->power = power;
+		head->next = NULL;
+		head->prev = NULL;
+	}
+	else if(power > cur->power)
+	{
+		head->prev = (Node*)malloc(sizeof(Node));
+		head->prev->prev = NULL;
+		head->prev->next = head;
+		head->prev->power = power;
+		head->prev->data = data;
+		head = head->prev;
+	}
+	else if(power == cur->power)
+	{
+		head->data += data;
+	}
+	else // power < cur->power
+	{
+		for(;cur->next != NULL && power < cur->next->power; cur = cur->next);
+		if(cur->next == NULL)
 		{
-			//printf("Ins: %dx^%d\n", curP1->data*curP2->data, curP1->power+curP2->power);
-			_insert(resultHead, curP1->data*curP2->data, curP1->power+curP2->power);
-			//printNode(resultHead);
+			cur->next = (Node*)malloc(sizeof(Node));
+			cur->next->next = NULL;
+			cur->next->prev = cur;
+			cur->next->data = data;
+			cur->next->power = power;
+		}
+		else if(power == cur->next->power)
+		{
+			cur->next->data += data;
+		}
+		else // power > cur->next->power
+		{
+			Node *newNode = (Node*)malloc(sizeof(Node));
+			newNode->data = data;
+			newNode->power = power;
+			newNode->prev = cur;
+			newNode->next = cur->next;
+			cur->next->prev = newNode;
+			cur->next = newNode;
 		}
 	}
+}
+
+Node* multiple(Node* p1, Node* p2)
+{
+	Node *cur1, *cur2, *resultHead = NULL;
+	
+	for(cur1 = p1; cur1 != NULL; cur1 = cur1->next)
+	for(cur2 = p2; cur2 != NULL; cur2 = cur2->next)
+		_insert(resultHead, cur1->data*cur2->data, cur1->power+cur2->power);
 	return resultHead;
 }
