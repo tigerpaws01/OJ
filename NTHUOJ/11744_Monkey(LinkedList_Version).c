f8f504cb3518a5659692a5aa75d1acdf6e06ff3c
tigerpaws01 <31960225+tigerpaws01@users.noreply.github.com>
2017-12-24 21:57:52 +0800

    Create 11744_Monkey(LinkedList_Version).c

---
+++ NTHUOJ/11744_Monkey(LinkedList_Version).c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _node{
+	char data;
+	struct _node *next, *prev;
+} Node;
+
+Node* NewNode(char data)
+{
+	Node *n = (Node*)malloc(sizeof(Node));
+	n->data = data;
+	n->next = n->prev = NULL;
+	return n;
+}
+void Insert(Node *pos, char data, Node **head)
+{
+	Node *n = NewNode(data);
+	// Attach
+	n->next = pos;
+	n->prev = pos->prev;
+	if(pos->prev != NULL) pos->prev->next = n;
+	pos->prev = n;
+	
+	// Modify head pointer when needed
+	if(n->prev == NULL) (*head) = n;
+}
+
+void Delete(Node **pos, Node **head)
+{
+	// Assertion
+	Node *cur = *pos, *d = *pos;
+	if(cur->next == NULL) return;
+	// Detach
+	if(cur->prev != NULL) cur->prev->next = cur->next;
+	cur->next->prev = cur->prev;
+	// Move Head
+	if(*pos == *head) *head = cur->next;
+	// Move cursor
+	*pos = cur->next;
+	
+	// Deallocate
+	free(d);
+}
+
+void LeftShift(Node **cursor)
+{
+	if((*cursor)->prev == NULL) return;
+	*cursor = (*cursor)->prev;
+}
+
+void RightShift(Node **cursor)
+{
+	if((*cursor)->next == NULL) return;
+	*cursor = (*cursor)->next;
+}
+
+void Print(Node *head)
+{
+	Node *cur = head;
+	while(cur != NULL)
+		putchar(cur->data), cur = cur->next;
+}
+
+void Free(Node *head)
+{
+	Node *cur = head;
+	while(cur != NULL)
+	{
+		Node *d = cur;
+		cur = cur->next;
+		free(d);
+	}
+}
+
+int main(){
+	// Create a tail node
+	Node *head = NewNode('\n'), *cursor = head;
+	char c;
+	while(~scanf(" %c", &c))
+	{
+		if(c == '>')
+			RightShift(&cursor);
+		else if(c == '<')
+			LeftShift(&cursor);
+		else if(c == 'D')
+			Delete(&cursor, &head);
+		else
+			Insert(cursor, c, &head);
+	}
+	
+	Print(head);
+	Free(head);
+	return 0;
+}
